3e07401056b4e1cb35df5db9ffd5e7b9a0bfc703
Qkauia <kauia96@gmail.com>
2023-09-23 14:27:57 +0800

    token input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
+
+    # validates ${:attribute}
     def index
     end
 
     def new
+    end
+
+    def create
+        render html: 123
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   # get '/', to: 'articles#index'
   root 'articles#index'
+  
   get '/articles', to: 'articles#index'
   get '/articles/new', to: 'articles#new'
+  post '/articles', to: 'articles#create'
 
   # resources :articles
 end
