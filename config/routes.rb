597c6f7b9ff214295e6526bda63db0b33bbbe5f4
Qkauia <kauia96@gmail.com>
2023-09-23 13:33:04 +0800

    routes add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/about', to: 'pages#about'
+  # get '/', to: 'articles#index'
+  root 'articles#index'
   get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new'
+
+  # resources :articles
 end
