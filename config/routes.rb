98d84f0956e3ad350c4afa0bc402f4a91a7f4c77
Qkauia <kauia96@gmail.com>
2023-09-24 20:59:53 +0800

    routes resources articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   # get '/', to: 'articles#index'
   root 'articles#index'
   
-  get '/articles', to: 'articles#index'
-  get '/articles/new', to: 'articles#new', as: 'new_article'
-  post '/articles', to: 'articles#create'
-  #單一頁面
-  get '/articles/:id', to: 'articles#show', as: 'article'
-  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
-  patch 'articles/:id', to: 'articles#update'
-  delete 'articles/:id', to: 'articles#destroy'
-  # resources :articles
+  # get '/articles', to: 'articles#index'
+  # get '/articles/new', to: 'articles#new', as: 'new_article'
+  # post '/articles', to: 'articles#create'
+  # #單一頁面
+  # get '/articles/:id', to: 'articles#show', as: 'article'
+  # get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  # patch 'articles/:id', to: 'articles#update'
+  # delete 'articles/:id', to: 'articles#destroy'
+  resources :articles
 end
