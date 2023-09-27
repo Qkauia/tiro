8904f2c2dba285dac2f3d8fccfda9cbe063dfe8b
Qkauia <kauia96@gmail.com>
2023-09-27 11:07:31 +0800

    目前登入保留記憶體至controller 老大給veiw共享

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < ActionController::Base
     #意外處理
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
-
+    #helper匯出給view使用
+    helper_method :user_signed_in?, :current_user
     private
+
+    def user_signed_in?
+        session[:xman].present?
+    end
+
+    def current_user
+        if user_signed_in?
+            #記憶
+            @__user__ ||= User.find_by(id: session[:xman])
+        else
+            nil
+        end
+    end
 
     def not_found
         render file: Rails.root.join('public', '404.html'),
