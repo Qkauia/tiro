43b8266d433aeeba997cbf653f6dacada1d41f83
Qkauia <kauia96@gmail.com>
2023-09-29 00:04:41 +0800

    stinulus 初步概念

---
+++ app/javascript/controllers/like_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="like"
+export default class extends Controller {
+  static targets = ["count"];
+  
+  hi(e){
+    e.preventDefault();
+    const currentCount = +this.countTarget.textContent
+    this.countTarget.textContent = currentCount + 1;
+
+    if (currentCount >= 5){
+      const o = { detail: { cc: currentCount}}
+      const evt = new CustomEvent("abc", o);
+      window.dispatchEvent(evt);
+    }
+  }
+}
