199429a9d3c514801c699d62da6a6493b4284236
Qkauia <kauia96@gmail.com>
2023-10-01 21:00:31 +0800

    add album controller,add tailwindcss

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem "paranoia", "~> 2.6"
+
+gem "tailwindcss-rails", "~> 2.0"
--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,30 @@
 class AlbumsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+
     def index
+        @albums = Album.order(id: :desc)
+    end
+
+    def new
+        @album = Album.new
+    end
+    
+    def create
+        @album = current_user.albums.new(album_params)
+        if @album.save
+            redirect_to albums_path(@album), notice: "相簿新增成功"
+        else
+            flash[:notice] = "相簿新增失敗"
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:name, :description, :password, :online)
     end
 end
