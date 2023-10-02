68bc7785b0421f6975742c8a78700e88740a7880
Qkauia <kauia96@gmail.com>
2023-10-02 11:19:45 +0800

    ??

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,8 @@
 class Album < ApplicationRecord
-  has_many_attached :photos
+  has_many_attached :photos do |attachable|
+    attachable.variant :small, resize_to_limit: [100, 100]
+  end
+
   belongs_to :user
 
 
